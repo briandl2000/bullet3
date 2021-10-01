53a99d7478e86e52f397f317161ebaffcf01f55d
briandl2000 <brian@delangecastricum.nl>
2021-10-01 14:26:53 +0200

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "Bullet"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
